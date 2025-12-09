db94bc12e5879147b4467fe8ec12927a5bfb1f0c
CooperProvins <cooperprovins@gmail.com>
2025-12-09 10:31:40 -0800

    Started main java interaction

--- code/Main.java
+++ code/Main.java
@@ -5,7 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Weapon myWeapon = new Weapon(15);
 
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("What character do you want to be?");
+        System.out.println("1. Scavenger\n2. Medic\n3. Warrior");
+        int role = input.nextInt();
+        while (role != 1 && role != 2 && role != 3) {
+            System.out.println("Invalid input. Please enter 1, 2, or 3.");
+            role = input.nextInt();
+        }
+
+        Person player;
+
+        switch (role) {
+            case 1:
+                player = new Scavenger(name);
+                break;
+            case 2:
+                player = new Medic(name);
+                break;
+            case 3:
+                player = new Warrior(name);
+                break;
+        }
+        for (int nights = 1; nights <= 10; nights++) {
+            
+        }
     }
 }
